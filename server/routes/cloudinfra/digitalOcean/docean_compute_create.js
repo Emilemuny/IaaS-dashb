00c1a8baba450824ce551f6f6af10e373744b593
emilemuny <emilemuny007@gmail.com>
2015-04-11 18:53:32 -0700

    adding code to get server Images/Flavors -working

--- server/routes/cloudinfra/digitalOcean/docean_compute_create.js
+++ server/routes/cloudinfra/digitalOcean/docean_compute_create.js
@@ -13,18 +13,25 @@ module.exports = {
       });
     });
 
-    var options = {
-      name: 'TestServer',
-      flavor: '2233',
-      image: '6370882',
-      region: 'nyc2'
-    };
-    client.createServer(options, function(err, server){
-      if(err){
-        console.log('There was an error', err);
-      }
-      console.log('Server created', server.toJSON());
+    client.getImages(function(err, images){
+      if(err){ console.log('Error', err); }
+      images.forEach(function(image){
+        console.log('Image', image);
+      });
     });
+
+    // var options = {
+    //   name: 'TestServer',
+    //   flavor: '64',
+    //   image: '64',
+    //   region: 'nyc2'
+    // };
+    // client.createServer(options, function(err, server){
+    //   if(err){
+    //     console.log('There was an error', err);
+    //   }
+    //   console.log('Server created', server.toJSON());
+    // });
 
     reply('Success');
   }
