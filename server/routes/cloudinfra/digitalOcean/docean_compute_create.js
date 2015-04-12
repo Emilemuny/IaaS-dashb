8375931d56c6dde88790b595d37f6f72d5586b23
emilemuny <emilemuny007@gmail.com>
2015-04-11 19:47:55 -0700

    Digital ocean create server working

--- server/routes/cloudinfra/digitalOcean/docean_compute_create.js
+++ server/routes/cloudinfra/digitalOcean/docean_compute_create.js
@@ -5,33 +5,32 @@ var client = require('../../../models/digitalOcean');
 module.exports = {
   auth: false,
   handler: function(request, reply){
-
-    client.getFlavors(function(err, flavors){
-      if(err){ console.log('Error', err);}
-      flavors.forEach(function(flavor){
-        console.log('Flavor', flavor);
-      });
-    });
-
-    client.getImages(function(err, images){
-      if(err){ console.log('Error', err); }
-      images.forEach(function(image){
-        console.log('Image', image);
-      });
-    });
-
-    // var options = {
-    //   name: 'TestServer',
-    //   flavor: '64',
-    //   image: '64',
-    //   region: 'nyc2'
-    // };
-    // client.createServer(options, function(err, server){
-    //   if(err){
-    //     console.log('There was an error', err);
-    //   }
-    //   console.log('Server created', server.toJSON());
+    // client.getFlavors(function(err, flavors){
+    //   if(err){ console.log('Error', err);}
+    //   flavors.forEach(function(flavor){
+    //     console.log('Flavor', flavor);
+    //   });
     // });
+
+    // client.getImages(function(err, images){
+    //   if(err){ console.log('Error', err); }
+    //   images.forEach(function(image){
+    //     console.log('Image', image);
+    //   });
+    // });
+
+    var options = {
+      name: 'node-v0.12.2 on 14.04',
+      flavor: '64',
+      image: '11388420',
+      region: '4'
+    };
+    client.createServer(options, function(err, server){
+      if(err){
+        console.log('There was an error', err);
+      }
+      console.log('Server created', server);
+    });
 
     reply('Success');
   }
